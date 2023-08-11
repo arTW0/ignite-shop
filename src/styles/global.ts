4f3a8c06d3bb2b11b9e0fc8ab135ba9b0b703e06
arTW0 <arthur.moreira@itec.ufpa.br>
2023-08-11 16:47:09 -0300

    configurando stitches

---
+++ src/styles/global.ts
@@ -0,0 +1,5 @@
+import { globalCss } from "@stitches/react";
+
+export const globalStyles = globalCss({
+
+})
---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  css,
+  theme,
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  createTheme
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6',
+    }
+  }
+})
