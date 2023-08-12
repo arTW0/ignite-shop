b2542f66417cbd1ee3a8afa4b92d0052655e01d3
arTW0 <arthur.moreira@itec.ufpa.br>
2023-08-11 21:20:10 -0300

    estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+import { Container, Header } from "../styles/pages/app";
+import logoImg from '../assets/logo.svg'
 
-export default MyApp
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Container>
+    <Header>
+      <img src={logoImg.src} alt="" />
+    </Header>
+
+    <Component {...pageProps} />
+  </Container>
+}
